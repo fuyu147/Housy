c3f6e85252993b289495af9787798c9dcaf4e3c7
Winter <fuyu.147@hotmail.com>
2025-12-26 18:55:52 -0500

    batman

---
+++ Assets/Code/move.fs
@@ -0,0 +1,31 @@
+namespace proj
+
+open UnityEngine
+open UnityEngine.InputSystem
+
+type Magnetude = float32
+
+type Movement() =
+    inherit MonoBehaviour()
+
+    [<SerializeField>]
+    let inputActions: InputActionAsset = null
+
+    let mutable move: InputAction = null
+
+    member _.OnEnable() = move.Enable()
+    member _.OnDisable() = move.Disable()
+
+    member _.Awake() =
+        let playerMap = inputActions.FindActionMap("PlayerMovement", true)
+
+        move <- playerMap.FindAction("Move", true)
+        ()
+
+    member this.Update() =
+        let mousePosition = Mouse.current.position.ReadValue()
+        Debug.Log <| sprintf "mouse position : <%f, %f>" mousePosition.x mousePosition.y
+
+        let movement = move.ReadValue<Vector2>()
+
+        Time.deltaTime * Vector3(movement.x, movement.y, 0f) |> this.transform.Translate
