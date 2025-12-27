4fdc5586dcf6575b04b9ff6c299bce5c8881835e
Winter <fuyu.147@hotmail.com>
2025-12-26 20:11:33 -0500

    added the start of the MoveCamera class

---
+++ Assets/Code/camera.fs
@@ -0,0 +1,15 @@
+namespace proj
+
+open UnityEngine
+
+type MoveCamera() =
+    inherit MonoBehaviour()
+
+    [<SerializeField>]
+    let movementSpeed = 100
+
+    [<SerializeField>]
+    let rotationSpeed = 10
+
+    member _.Start () = ()
+    member _.Update () = ()
